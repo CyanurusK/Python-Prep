0578a3413967e331edd4a14f3753d2f7736f03cb
Joel <cyanurusk@gmail.com>
2022-08-18 02:55:58 -0600

    Tarea1

---
+++ 02 - Variables y Tipos de Datos/Tarea.py
@@ -0,0 +1,95 @@
+#1
+numero = 5
+print(numero)
+
+#2
+print(type(8.5))
+
+#3
+print(type(numero))
+
+#4
+nombre = "Joel"
+
+#5
+complejo = 4+9j
+
+#6
+print(type(complejo))
+
+#7
+pi = 3.1416
+
+#8
+a = 'True'
+b = True
+
+#9
+print(type(a))
+print(type(b))
+
+#10
+suma = 3 + 4.5
+
+#11
+num1 = 9 + 5j
+num2 = 7 + 4j
+print(num1 + num2)
+
+#12
+print(num1 + 4)
+
+#13
+print(5*7)
+
+#14
+print(2**8)
+
+#15
+c = 27/4
+print(c)
+
+#16
+d = 27//4
+print(d)
+
+#17
+e = 27 % 4
+print(e)
+
+#18
+print(4*d+e)
+
+#19
+f = "Hola "
+g = "Mundo"
+print(f + g)
+
+#20
+print("2" == 2)
+
+#21
+print("2" == str(2))
+
+#22
+a = float("3.8")
+print(a)
+#Se cambió la coma decimal por un punto decimal
+
+#23
+a = 3
+a -= 1
+print(a)
+
+#24
+print(1 << 2)
+#El operador << desplaza el valor binario la cantidad de veces del siguiente número a la izquierda, convirtiendo el 1 en 100, cuyo valor en decimal es 4
+
+#25
+print(str(2) + '2')
+print(2 + int('2'))
+
+#26
+texto = "Hola Mundo\n"
+i = 4
+print(texto*i)
